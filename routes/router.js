6086a3dbe3f92b0c205bbebc94c84d4d068732cb
rushikeshdixit <rushikesh.rush@gmail.com>
2018-06-19 14:17:12 -0500

    Adding delete functionality

--- routes/router.js
+++ routes/router.js
@@ -18,17 +18,15 @@ router.post('/note', function (req, res, next) {
                 error: err
             });
         }
-        res.redirect('/')
+        res.status(200).redirect('/')
     });
 });
 
-router.get('/notes', function (req, res, next) {
-    console.log('I am here')
+router.get('/note', function (req, res, next) {
     Note.find()
         //.populate('title', 'content')
         .exec(function (err, messages) {
             if (err) {
-                console.log('I am here')
                 return res.status(500).json({
                     title: 'An error occurred',
                     error: err
@@ -39,6 +37,22 @@ router.get('/notes', function (req, res, next) {
                 obj: messages
             });
         });
+});
+
+router.delete('/note/:id', function(req, res, next){
+    var id = req.params.id;
+    console.log("BODY" + JSON.stringify(req.params) + " " + id);
+    Note.findByIdAndRemove(id, function(err,message) {
+        console.log("ERROR:" + err);
+        if(err) {
+            console.log("DID ERRORED")
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        res.status(200).redirect('/');
+    })
 });
 
 module.exports = router;
